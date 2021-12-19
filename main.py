42827c6927ad870879e76c552f4c8ff80d28a5d0
Minengp <96377674+Minengp@users.noreply.github.com>
2021-12-19 17:43:55 +0500

    Update main.py
    lol

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('LOl')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
